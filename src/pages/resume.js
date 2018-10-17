dde41fb68b4a90516e1a64a16b6dbb4522acdca1
Justin Nitz <8098188+ionicpanda@users.noreply.github.com>
2018-10-16 19:16:18 -0500

    Update resume.js

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,6 +4,6 @@ import Layout from "../components/layout"
 export default () => (
   <Layout>
     <h1>Resume</h1>
-    <p>My Resume. Will be completed someday. Maybe generate a pdf static? or markdown...</p>
+    <p>Placeholder for resume</p>
   </Layout>
 )
