57226be07429b606d9270a7066d6a17ff74b25bd
Justin Nitz <8098188+ionicpanda@users.noreply.github.com>
2018-10-16 19:15:45 -0500

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,6 +4,6 @@ import Layout from "../components/layout"
 export default () => (
   <Layout>
     <h1>Projects</h1>
-    <p>Create a list/columns of projects</p>
+    <p>Needs to be filled with some projects!</p>
   </Layout>
 )
