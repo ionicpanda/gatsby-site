3452ba36c990d6d5797a949e89e147d96a358f60
Justin Nitz <8098188+ionicpanda@users.noreply.github.com>
2018-10-19 17:49:58 -0500

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,6 +4,6 @@ import Layout from "../components/layout"
 export default () => (
   <Layout>
     <h1>Projects</h1>
-    <p>Needs to be filled with some projects!</p>
+    <p>I'm sorry, but this page is currently under construction. It will be completed sometime in the future.</p>
   </Layout>
 )
