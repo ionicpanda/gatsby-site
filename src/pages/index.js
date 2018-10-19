ee881baa5ffcdd20ef02b308804ac01afd88ae6d
Justin Nitz <8098188+ionicpanda@users.noreply.github.com>
2018-10-19 17:49:35 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-    <h1>justin nitz</h1>
-    <p>I'm a computer science student studying at the University of Central Missouri. Most of my time is spent learning all aspects of software engineering. I have a passion for computer security and like combining by love of development with security.</p>
+    <h2>About Me</h2>
+    <p>I'm a computer science student at the University of Central Missouri.
+       I strive to learn as much as I can about programming and computer security. When I'm not studying computer science, I can be found playing the french horn in my University's orchestra. 
+I believe that balancing my life between mathematics and music allows me to strenghten both hemispheres of my brain. I've spent a few years working in the infosec community and now I strive on becoming a software engineer who utilizes secure coding paradigms.</p>  
     </Layout>
 )
